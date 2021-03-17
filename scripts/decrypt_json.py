c3a3a60c4176a78bb8f4af34ce67989810e93b33
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2021-03-17 15:30:19 -0700

    Access drive secret directly

--- scripts/decrypt_json.py
+++ scripts/decrypt_json.py
@@ -1,8 +1,4 @@
 import os
 import sys
 
-password = sys.argv[1]
-print(os.listdir())
-print(len(password[0]))
-print(password[0])
 os.system(f"""gpg --quiet --batch --yes --decrypt --passphrase="{os.environ['DRIVE_SECRET']}" --output token.json token.json.gpg""")
