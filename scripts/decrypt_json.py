d7188519b326647174be3dcc68cd8784506abc9c
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2021-03-17 08:29:42 -0700

    Update decrypt_json.py

--- scripts/decrypt_json.py
+++ scripts/decrypt_json.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 password = sys.argv[1]
-os.chdir('..')
+print(os.listdir())
 os.system(f"""gpg --quiet --batch --yes --decrypt --passphrase="{password}" --output token.json token.json.gpg""")
