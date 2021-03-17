bba423f9300152a4907ce63962f27d3de7a5be65
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2021-03-17 11:39:36 -0700

    Update decrypt_json.py

--- scripts/decrypt_json.py
+++ scripts/decrypt_json.py
@@ -3,4 +3,5 @@ import sys
 
 password = sys.argv[1]
 print(os.listdir())
+print(os.environ['DRIVE_SECRET'] == password)
 os.system(f"""gpg --quiet --batch --yes --decrypt --passphrase="{password}" --output token.json token.json.gpg""")
