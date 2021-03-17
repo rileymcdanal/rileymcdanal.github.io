8fc33c0831efb20edc93beff943e5a16087f442f
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2021-03-16 22:18:29 -0700

    include token name in decryption

--- decrypt_json.py
+++ decrypt_json.py
@@ -3,4 +3,4 @@ import sys
 
 password = sys.argv[1]
 
-os.system(f"""gpg --quiet --batch --yes --decrypt --passphrase="{password}" """)
+os.system(f"""gpg --quiet --batch --yes --decrypt --passphrase="{password}" --output token.json token.json.gpg""")
