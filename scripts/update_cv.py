127df3434247c033de39ced4bbc985a0f485034c
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2024-06-24 15:55:29 -0400

    Update update_cv.py

--- scripts/update_cv.py
+++ scripts/update_cv.py
@@ -28,8 +28,8 @@ def replace_number_in_pdf(input_pdf_path, output_pdf_path, old_number, new_numbe
         for page_num in range(len(reader.pages)):
             page = reader.pages[page_num] 
             content = page.extract_text()
-            updated_content = content.replace(str(old_number), str(new_number))
-
+            # updated_content = content.replace(str(old_number), str(new_number))
+            updated_content = content.replace('6.24.24', str(new_number))
             # Write the updated content to a new page
             new_page = writer.add_blank_page(width=page.mediabox.width, height=page.mediabox.height)
             new_page.merge_page(page)
